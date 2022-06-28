cd7759896d931a8ec8e4c7361756e3768031978a
SlavaMonochrom <107564935+SlavaMonochrom@users.noreply.github.com>
2022-06-29 00:44:07 +0300

    Lectiontwo

--- LectionOne/Task3/Program.cs
+++ LectionOne/Task3/Program.cs
@@ -7,3 +7,4 @@ int numSecond = Convert.ToInt32(Console.ReadLine());
 int sum = 0;
 sum = numFirsrt + numSecond;
 Console.WriteLine("Сумма двух чисел равна: " + sum);
+
---
+++ LectionOne/Task4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите первое число: ");
+double numFirsrt = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+double numSecond = Convert.ToDouble(Console.ReadLine());
+
+double result;
+result = (numFirst / numSecond);
+Convert.ToDouble(Console.WriteLine("first div second: " + result));
