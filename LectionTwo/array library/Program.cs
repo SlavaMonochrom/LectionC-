56af9dd1fcf061d8d0ef03a60e5edc33abf88de8
SlavaMonochrom <107564935+SlavaMonochrom@users.noreply.github.com>
2022-06-29 04:00:21 +0300

    Lectiontwo

---
+++ LectionTwo/array library/Program.cs
@@ -0,0 +1,46 @@
+﻿void FillArray(int[] collection)
+{
+    int lenght = collection.Length;
+    int index = 0;
+    while(index<lenght)
+    {
+        collection[index] = new Random().Next(1,10);
+         index++;
+    }
+} 
+void Printarray(int[]col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position<count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int IndexOFf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = 0;
+
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+        } 
+        index++;
+
+    }
+    return position;
+}
+
+int[] array = new int[10];
+FillArray(array);
+Printarray(array);
+Console.WriteLine();
+
+int pos = IndexOFf(array,5);
+Console.WriteLine(pos); 
